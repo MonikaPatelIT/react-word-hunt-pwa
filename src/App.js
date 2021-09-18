858bd69c919ca059e772b5c60d975d8184c94ca8
Monika Patel <monika.patel@homecaremedical.co.nz>
2021-09-19 10:53:46 +1200

    replace mui 5 with mui 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import Container from "@material-ui/core/Container";
 import "./App.css";
+import { Header } from "./components/Header/Header";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en");
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div
+      className="App"
+      style={{ height: "100vh", backgroundColor: "#282c34", color: "white" }}
+    >
+      <Container
+        maxWidth="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "center",
+        }}
+      >
+        <Header />
+      </Container>
     </div>
   );
 }
